f6a20278354003cf7b435bc8ff9098cb7ca6aecd
Lucas <lucassantosamaral@hotmail.com>
2018-03-17 17:16:14 -0300

    Instalando as Dependências e Configurando os Reducers

---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux'
+
+const roorReducer = combineReducers({
+    todo: () => ({
+        description: 'Ler livro',
+        list: [{
+            _id: 1,
+            description: 'Pagar fatura do cartão',
+            done: true
+        },{
+            _id: 2,
+            description: 'Reunião com a equipe às 10:00',
+            done: false
+        },{
+            _id: 3,
+            description: 'Consulta média na terça depois do almoço',
+            done: false
+        }]
+    })
+})
+
+export default rootReducer
